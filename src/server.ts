6011c2c556af4f502bb9b5e562f0dc95305f767d
Alper <alperemreeren@hotmail.com>
2022-05-26 14:32:18 +0300

    refactor: changes for main file

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Importing Resolvers and Type Defs.
+import { typeDefs } from "./graphql/typeDefs";
+import { resolvers } from "./graphql/resolvers";
+
+// Types
+import { Request, Response } from "express";
+
+const startServer: Function = async () => {
+  const app = express();
+  const apolloServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+      csrfPrevention: true,
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app: app });
+
+  app.use((req: Request, res: Response) => {
+    res.send("It Works!");
+  });
+
+  // Dynamic connection string changeable from .env file for convenience
+  await mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}${process.env.DB_PARAMS}`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+  console.log('Connected to MongoDB 💾')
+
+  app.listen(process.env.PORT, () => console.log(`🌐 Listening on URL: http://localhost:${process.env.PORT}`));
+}
+startServer();
