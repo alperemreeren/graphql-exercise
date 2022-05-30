ea708e881247a62a768f2aed160939297dd6cff0
Alper <alperemreeren@hotmail.com>
2022-05-30 11:52:52 +0300

    added CORS

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 // Importing Resolvers and Type Defs.
@@ -13,17 +14,21 @@ import { Request, Response } from "express";
 const startServer: Function = async () => {
   const app = express();
   const apolloServer = new ApolloServer({
-      typeDefs,
-      resolvers,
-      csrfPrevention: true,
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
   });
-
+  
   await apolloServer.start();
   apolloServer.applyMiddleware({ app: app });
-
+  
+  // Allow Cross-Origin Resource Sharing
+  app.use(cors())
+  
   app.use((req: Request, res: Response) => {
     res.send("It Works!");
   });
+
 
   // Dynamic connection string changeable from .env file for convenience
   await mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}${process.env.DB_PARAMS}`, {
